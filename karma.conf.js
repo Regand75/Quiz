0cd5e4a97830910aad7d078044e95434a738b04e
regand75 <regandess@gmail.com>
2025-03-01 10:24:07 +0300

    start project

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/quiz-angular'),
+      dir: require('path').join(__dirname, './coverage/12-19-quiz-angular'),
       subdir: '.',
       reporters: [
         { type: 'html' },
