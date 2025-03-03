c7d598a3fb30be1ab4cfe95c7ab8dcd8901fd164
Regand75 <regandess@gmail.com>
2025-03-03 14:39:28 +0300

    creating registration

--- src/app/views/user/user.module.ts
+++ src/app/views/user/user.module.ts
@@ -4,9 +4,9 @@ import { CommonModule } from '@angular/common';
 import { UserRoutingModule } from './user-routing.module';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
-import {ReactiveFormsModule} from "@angular/forms";
-import {RouterModule} from "@angular/router";
-import {MAT_SNACK_BAR_DEFAULT_OPTIONS, MatSnackBarModule} from "@angular/material/snack-bar";
+import { ReactiveFormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
+import { MAT_SNACK_BAR_DEFAULT_OPTIONS } from "@angular/material/snack-bar";
 
 
 @NgModule({
@@ -18,11 +18,8 @@ import {MAT_SNACK_BAR_DEFAULT_OPTIONS, MatSnackBarModule} from "@angular/materia
     CommonModule,
     RouterModule,
     ReactiveFormsModule,
-    MatSnackBarModule,
     UserRoutingModule
   ],
-  providers: [
-    {provide: MAT_SNACK_BAR_DEFAULT_OPTIONS, useValue: {duration: 2500}}
-  ]
+  providers: []
 })
 export class UserModule { }
